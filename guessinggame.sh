58901223d941e45ba50ace1e56825bb347d0cc32
kszup <k.szupinski@gmail.com>
2018-04-22 10:40:11 -0400

    Added lowhigh function.

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,6 +2,19 @@
 # File: guessinggame.sh
 # Author: kszup
 
+function lowhigh {
+  # Function takes two arguments: guess and number of files
+  if [[ $1 -lt $2 ]]
+  then
+    echo "The guess is too low! Try again."
+  elif [[ $1 -gt $2 ]]
+  then
+    echo "The guess is too high! Try again."
+  else
+    echo "Error"
+  fi
+}
+ 
 
 echo "Hello! Guess how many files are in this directory. No peaking!"
 echo -n "Guess: "
@@ -9,11 +22,12 @@ read guess
 #echo $guess
 
 number_of_files=$(ls | wc -w) # ls -a | wc -w
-#echo $number_of_files
+echo $number_of_files # For testing
 
 while [[ $guess -ne $number_of_files ]]
 do
-  echo "The guess of $guess was INCORRECT! Try again."
+  #echo "The guess of $guess was INCORRECT! Try again."
+  lowhigh $guess $number_of_files
   echo -n "Guess how many files are in this directory: "
   read guess
 done
